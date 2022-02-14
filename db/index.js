99f736b10e569e5d807f800bbbe1a57afac2ddd8
Maxwell Dubin <maxdhs@gmail.com>
2022-02-14 12:25:12 -0500

    part 2

--- db/index.js
+++ db/index.js
@@ -10,7 +10,7 @@ const client = new Client({
 
 async function getAllUsers() {
   const { rows } = await client.query(
-    `SELECT id, username 
+    `SELECT id, username, name, location, active
       FROM users;
     `
   );
@@ -18,26 +18,132 @@ async function getAllUsers() {
   return rows;
 }
 
-async function createUser({ username, password }) {
+async function createUser({ username, password, name, location }) {
   try {
     const { rows } = await client.query(
       `
-      INSERT INTO users(username, password) 
-      VALUES($1, $2) 
+      INSERT INTO users(username, password, name, location) 
+      VALUES($1, $2, $3, $4) 
       ON CONFLICT (username) DO NOTHING 
       RETURNING *;
       `,
-      [username, password]
+      [username, password, name, location]
     );
 
-    return rows;
+    return rows[0];
   } catch (error) {
     throw error;
   }
+}
+
+async function createPost({ authorId, title, content }) {
+  try {
+    const { rows } = await client.query(
+      `
+      INSERT INTO posts("authorId", title, content) 
+      VALUES($1, $2, $3) 
+      RETURNING *;
+      `,
+      [authorId, title, content]
+    );
+
+    return rows[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUser(id, fields) {
+  for (let key of Object.keys(fields)) {
+    await client.query(
+      `
+    UPDATE users SET ${key} = $1
+    WHERE id = $2
+    RETURNING *;
+    `,
+      [fields[key], id]
+    );
+  }
+  const response = await client.query(
+    `
+  SELECT * FROM users
+  WHERE id = $1;
+  `,
+    [id]
+  );
+  return response.rows[0];
+}
+
+async function updatePost(id, fields) {
+  for (let key of Object.keys(fields)) {
+    await client.query(
+      `
+    UPDATE posts SET ${key} = $1
+    WHERE id = $2
+    RETURNING *;
+    `,
+      [fields[key], id]
+    );
+  }
+  const response = await client.query(
+    `
+  SELECT * FROM posts
+  WHERE id = $1;
+  `,
+    [id]
+  );
+  return response.rows[0];
+}
+
+async function getAllPosts() {
+  const { rows } = await client.query(
+    `SELECT *
+      FROM posts;
+    `
+  );
+
+  return rows;
+}
+
+async function getPostsByUser(id) {
+  const { rows } = await client.query(
+    `SELECT *
+      FROM posts
+      WHERE "authorId" = $1
+      ;
+    `,
+    [id]
+  );
+
+  return rows;
+}
+
+async function getUserById(id) {
+  const {
+    rows: [user],
+  } = await client.query(
+    `SELECT id, username, name, location
+      FROM users
+      WHERE id = $1
+      ;
+    `,
+    [id]
+  );
+
+  const posts = await getPostsByUser(id);
+  user.posts = posts;
+
+  return user;
 }
 
 module.exports = {
   client,
   getAllUsers,
   createUser,
+  updateUser,
+  createPost,
+  updatePost,
+  getAllPosts,
+  getPostsByUser,
+  getUserById,
 };
--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,21 @@
-const { client, getAllUsers, createUser } = require("./index");
+const {
+  client,
+  getAllUsers,
+  createUser,
+  updateUser,
+  createPost,
+  updatePost,
+  getAllPosts,
+  getPostsByUser,
+  getUserById,
+} = require("./index");
 
 async function dropTables() {
   try {
-    console.log("Starting to drop tables...");
-
     await client.query(`
+        DROP TABLE IF EXISTS posts;
         DROP TABLE IF EXISTS users;
       `);
-
-    console.log("Finished dropping tables!");
   } catch (error) {
     console.error("Error dropping tables!");
     throw error;
@@ -17,17 +24,24 @@ async function dropTables() {
 
 async function createTables() {
   try {
-    console.log("Starting to build tables...");
-
     await client.query(`
         CREATE TABLE users (
           id SERIAL PRIMARY KEY,
           username varchar(255) UNIQUE NOT NULL,
-          password varchar(255) NOT NULL
+          password varchar(255) NOT NULL,
+          name VARCHAR(255) NOT NULL,
+          location VARCHAR(255) NOT NULL,
+          active BOOLEAN DEFAULT true
         );
-      `);
 
-    console.log("Finished building tables!");
+        CREATE TABLE posts(
+          id SERIAL PRIMARY KEY,
+          "authorId" INTEGER REFERENCES users(id) NOT NULL,
+          title VARCHAR(255) NOT NULL,
+          content TEXT NOT NULL,
+          active BOOLEAN Default true
+        )
+      `);
   } catch (error) {
     console.error("Error building tables!");
     throw error;
@@ -36,28 +50,59 @@ async function createTables() {
 
 async function createInitialUsers() {
   try {
-    console.log("Starting to create users...");
-
     const albert = await createUser({
       username: "albert",
       password: "bertie99",
+      name: "fred",
+      location: "red bank",
     });
 
     const sandra = await createUser({
       username: "sandra",
       password: "bertie99",
+      name: "susan",
+      location: "LA",
     });
 
     const glamgal = await createUser({
       username: "glamgal",
       password: "bertie99",
+      name: "Plant",
+      location: "Anchorage",
     });
-
-    console.log(albert);
-
-    console.log("Finished creating users!");
   } catch (error) {
     console.error("Error creating users!");
+    throw error;
+  }
+}
+
+async function createInitialPosts() {
+  try {
+    const [albert, sandra, glamgal] = await getAllUsers();
+
+    await createPost({
+      authorId: albert.id,
+      title: "First Post",
+      content:
+        "This is my first post. I hope I love writing blogs as much as I love writing them.",
+    });
+
+    await createPost({
+      authorId: sandra.id,
+      title: "First Post",
+      content:
+        "This is my first post. I hope I love writing blogs as much as I love writing them.",
+    });
+
+    await createPost({
+      authorId: glamgal.id,
+      title: "First Post",
+      content:
+        "This is my first post. I hope I love writing blogs as much as I love writing them.",
+    });
+
+    // a couple more
+  } catch (error) {
     throw error;
   }
 }
@@ -69,6 +114,7 @@ async function rebuildDB() {
     await dropTables();
     await createTables();
     await createInitialUsers();
+    await createInitialPosts();
   } catch (error) {
     throw error;
   }
@@ -76,14 +122,25 @@ async function rebuildDB() {
 
 async function testDB() {
   try {
-    console.log("Starting to test database...");
-
     const users = await getAllUsers();
-    console.log("getAllUsers:", users);
 
-    console.log("Finished database tests!");
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lesterville, KY",
+    });
+
+    const posts = await getAllPosts();
+
+    const updatePostResult = await updatePost(posts[0].id, {
+      title: "New Title",
+      content: "Updated Content",
+    });
+
+    const albert = await getUserById(1);
+
+    console.log("db seeded.");
   } catch (error) {
-    console.error("Error testing database!");
+    console.log("Error during testDB");
     throw error;
   }
 }
